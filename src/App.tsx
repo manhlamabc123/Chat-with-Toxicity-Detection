2f6cf972972f8714b34dd94741730425bfc1d354
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-03-28 08:57:09 +0700

    Clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,99 @@
-import { useEffect, useState, useRef } from "react";
-import * as tf from "@tensorflow/tfjs";
-import { load } from "@tensorflow-models/toxicity";
-
-// Set an id counter, so that we can automatically identify each message with a unique number:
-let id = 3;
-// Initialize our messages list with some sample messages:
-const initialMessages = [
-  { id: 1, msg: "Hello!" },
-  { id: 2, msg: "What's up?" },
-  { id: 3, msg: "Hello!" },
-];
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBIcon,
+  MDBBtn,
+  MDBTypography,
+  MDBTextArea,
+  MDBCardHeader,
+} from "mdb-react-ui-kit";
 
 export default function App() {
-  const [messages, setMessages] = useState(initialMessages); // The list of messages
-  const [toxicity, setToxicity] = useState({ isToxic: false, labels: [] }); // The state of toxicity for the message we just typed
-  const [isClassifying, setIsClassifying] = useState(false); // A simple state variable to reflect the classifying process status
-  const [hasLoaded, setHasLoaded] = useState(false); // Has the Toxicity model been loaded?
-  const hasMessages = messages.length > 0; // Should we render the ul that will hold the messages if no messages are available?
-  const model = useRef<any | null>(null); // Retain a value throughout the Component's render cycles WITHOUT triggering a render, as opposed to a useState variable
-
-  // Load the model just one, when the component mounts (hence the empty dependency array [])
-  useEffect(() => {
-    async function loadModel() {
-      const threshold = 0.9;
-      // Set a state that indicates the model is being loaded...
-      model.current = await load(threshold, []);
-      setHasLoaded(true);
-      // Set the state to false to let the user know that they can check the text
-      console.log("Model loaded");
-    }
-    loadModel();
-  }, []);
-
-  // Handle form submission: check the toxicity of the message and update accordingly:
-  const sendMessage = async (event: any) => {
-    // console.log("sendMessage()");
-    event.preventDefault(); // Prevent default HTML form behavior that will trigger an HTTP request and a page reload
-    const form = event.target;
-    const msg = form.message.value;
-
-    // Run the classifier on every message
-    setIsClassifying(true);
-    const predictions = await model.current.classify([msg]);
-    setIsClassifying(false);
-
-    // Is the message toxic?
-    const isToxic = predictions[6].results[0].match;
-
-    if (isToxic) {
-      const labels: any = [];
-      // Loop through the toxicity labels and create a list of them along with the corresponding percentages (level of confidence):
-      predictions.forEach((p: any) => {
-        if (p.results[0].match) {
-          labels.push({
-            label: p.label,
-            prob: Math.round(p.results[0].probabilities[1] * 100) + "%",
-          });
-        }
-      });
-      // console.log(labels);
-      setToxicity({ isToxic: true, labels });
-    } else {
-      setMessages([...messages, { id: ++id, msg: msg }]);
-      setToxicity({ isToxic: false, labels: [] });
-      form.reset();
-    }
-  };
-
   return (
-    <>
-      <h1>TensorFlow.JS + Toxicity Classifier</h1>
-      {hasMessages && (
-        <ul>
-          {messages.map((message) => {
-            return <li key={message.id}>{message.msg}</li>;
-          })}
-        </ul>
-      )}
-      {hasLoaded && (
-        <form onSubmit={sendMessage}>
-          <input type="text" name="message" placeholder="Enter message" />
-          <button>{isClassifying ? " ️‍️‍🕵 " : "Send"}</button>
-          <span>{toxicity.isToxic && " 🤐"}</span>
-          {toxicity.labels.map((l: any) => ` ${l.label} ${l.prob}`)}
-        </form>
-      )}
-    </>
+    <MDBContainer fluid className="py-5" style={{ backgroundColor: "#eee" }}>
+      <MDBRow>
+        <MDBCol md="6" lg="8" xl="12">
+          <MDBTypography listUnStyled>
+            <li className="d-flex justify-content-between mb-4">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+                alt="avatar"
+                className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
+                width="60"
+              />
+              <MDBCard>
+                <MDBCardHeader className="d-flex justify-content-between p-3">
+                  <p className="fw-bold mb-0">Brad Pitt</p>
+                  <p className="text-muted small mb-0">
+                    <MDBIcon far icon="clock" /> 12 mins ago
+                  </p>
+                </MDBCardHeader>
+                <MDBCardBody>
+                  <p className="mb-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </MDBCardBody>
+              </MDBCard>
+            </li>
+            <li className="d-flex justify-content-between mb-4">
+              <MDBCard className="w-100">
+                <MDBCardHeader className="d-flex justify-content-between p-3">
+                  <p className="fw-bold mb-0">Lara Croft</p>
+                  <p className="text-muted small mb-0">
+                    <MDBIcon far icon="clock" /> 13 mins ago
+                  </p>
+                </MDBCardHeader>
+                <MDBCardBody>
+                  <p className="mb-0">
+                    Sed ut perspiciatis unde omnis iste natus error sit
+                    voluptatem accusantium doloremque laudantium.
+                  </p>
+                </MDBCardBody>
+              </MDBCard>
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"
+                alt="avatar"
+                className="rounded-circle d-flex align-self-start ms-3 shadow-1-strong"
+                width="60"
+              />
+            </li>
+            <li className="d-flex justify-content-between mb-4">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+                alt="avatar"
+                className="rounded-circle d-flex align-self-start me-3 shadow-1-strong"
+                width="60"
+              />
+              <MDBCard>
+                <MDBCardHeader className="d-flex justify-content-between p-3">
+                  <p className="fw-bold mb-0">Brad Pitt</p>
+                  <p className="text-muted small mb-0">
+                    <MDBIcon far icon="clock" /> 10 mins ago
+                  </p>
+                </MDBCardHeader>
+                <MDBCardBody>
+                  <p className="mb-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p>
+                </MDBCardBody>
+              </MDBCard>
+            </li>
+            <li className="bg-white mb-3">
+              <MDBTextArea label="Message" id="textAreaExample" rows={4} />
+            </li>
+            <MDBBtn color="info" rounded className="float-end">
+              Send
+            </MDBBtn>
+          </MDBTypography>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
